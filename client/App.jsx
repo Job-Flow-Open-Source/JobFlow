39000e02d9a60775bc31001e5bd46bd7d5972c57
Jonathan Wong <106034697+WongJonathann@users.noreply.github.com>
2023-01-30 16:30:04 -0500

    this will start up repo for JobFlow

---
+++ client/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+// import Signup from './components/Signup';
+// import Login from './components/Login';
+// import Home from './components/Home';
+
+const App = (props) => {
+  return (
+    <div className="router">
+      {/* content seen on every page */}
+      <div className="routerMain" id='content'>
+        <Routes>
+          {/* different path inside each rout, component inside- loggin, sign up, home */}
+          {/* <Route path='/' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/home' element={<Home />} /> */}
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
