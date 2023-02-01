e489f7216642ba904f9be906c5a0b92002396ba4
hwangja1019@gmail.com <hwangja1019@gmail.com>
2023-02-01 12:09:15 -0500

    added default entries to edit modal

--- client/components/AppEntry.jsx
+++ client/components/AppEntry.jsx
@@ -1,9 +1,8 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import Modal from './Modal';
 
 const AppEntry = (props) => {
-    
-    /*
+  /*
         0 = started application
         20 = submitted application 
         40 = phone screening
@@ -12,27 +11,49 @@ const AppEntry = (props) => {
         100 = offer 
             (accept or rejected)
     */
-
-    return (
-        <tr> 
-            <td> {props.index + 1} </td>
-            <td> {props.appInfo.company} </td>
-            <td> {props.appInfo.job_title} </td>
-            <td> {props.appInfo.link} </td>
-            <td> {props.appInfo.submission_method} </td>
-            <td> {props.appInfo.date_submitted} </td>
-            <td> {props.appInfo.resume_id} </td>
-            <td> {props.appInfo.cover_letter_status} </td>
-            <td> <progress id="file" max="100" value={props.appInfo.progress_status}></progress> </td>
-            <td> <button onClick={(e) => {props.setShow(true); console.log('edit clicked')}}>edit</button> </td>
-            <Modal 
-                onClose={() => { return props.setShow(false);}}
-                show={props.show}
-                closeModal={() => {setShowEdit(false)}}
-                id='editModal'
-            />
-        </tr>   
-    )
-}
+  return (
+    <tr>
+      <td> {props.index + 1} </td>
+      <td> {props.appInfo.company} </td>
+      <td> {props.appInfo.job_title} </td>
+      <td> {props.appInfo.link} </td>
+      <td> {props.appInfo.submission_method} </td>
+      <td> {props.appInfo.date_submitted} </td>
+      <td> {props.appInfo.resume_id} </td>
+      <td> {props.appInfo.cover_letter_status} </td>
+      <td>
+        {' '}
+        <progress
+          id='file'
+          max='100'
+          value={props.appInfo.progress_status}
+        ></progress>{' '}
+      </td>
+      <td>
+        {' '}
+        <button
+          onClick={(e) => {
+            props.setShow(true);
+            console.log('edit clicked', props.modalState);
+            props.setModalState('edit');
+            props.setIdx(props.index);
+          }}
+        >
+          edit
+        </button>{' '}
+      </td>
+      {/* <Modal
+        onClose={() => {
+          return props.setShow(false);
+        }}
+        show={props.show}
+        closeModal={() => {
+          setShowEdit(false);
+        }}
+        id={props.modalState}
+      /> */}
+    </tr>
+  );
+};
 
 export default AppEntry;
